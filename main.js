3e0ce58f4e242ab3be9e377e2221fd311ac9b338
Hazrat Ali <iloveyii@yahoo.com>
2019-01-03 10:34:44 +0100

    Initial commit with main.js and vue.html set

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+
+const path = require('path');
+
+const url = require('url');
+
+// init
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({width: 800, height:600, icon: __dirname + '/favicon.png'});
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'vue.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    // open dev tools
+    win.webContents.openDevTools();
+    win.on('closed', () => {
+        win = null;
+    });
+}
+
+// Create
+app.on('ready', createWindow);
+
+// Close
+app.on('window-all-closed', ()=> {
+    if(process.platform !== 'darwin') {
+        app.quit();
+    }
+});
