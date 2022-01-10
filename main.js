4acf3fa21248da4727dec26b61993d258e6df403
Hazrat Ali <iloveyii@yahoo.com>
2022-01-10 13:44:22 +0100

    System tray added for Ubuntu

--- main.js
+++ main.js
@@ -1,33 +1,49 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Tray, Menu } = require("electron");
 
-const path = require('path');
+const path = require("path");
 
-const url = require('url');
+const url = require("url");
 
 // init
 let win;
 
 function createWindow() {
-    win = new BrowserWindow({width: 1600, height:1200, icon: __dirname + '/app/assets/img/favicon.png'});
-    win.loadURL(url.format({
-        pathname: path.join(__dirname, 'player.html'),
-        protocol: 'file',
-        slashes: true
-    }));
+  win = new BrowserWindow({
+    width: 1600,
+    height: 1200,
+    icon: __dirname + "/app/assets/img/favicon.png",
+  });
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "player.html"),
+      protocol: "file",
+      slashes: true,
+    })
+  );
 
-    // open dev tools
-    // win.webContents.openDevTools();
-    win.on('closed', () => {
-        win = null;
-    });
+  let tray = new Tray("/home/ali/projects/java/ePlayer/favicon.png");
+  const contextMenu = Menu.buildFromTemplate([
+    { label: "Item1", type: "radio" },
+    { label: "Item2", type: "radio" },
+    { label: "Item3", type: "radio", checked: true },
+    { label: "Item4", type: "radio" },
+  ]);
+  tray.setToolTip("This is my application.");
+  tray.setContextMenu(contextMenu);
+
+  // open dev tools
+  // win.webContents.openDevTools();
+  win.on("closed", () => {
+    win = null;
+  });
 }
 
 // Create
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
 // Close
-app.on('window-all-closed', ()=> {
-    if(process.platform !== 'darwin') {
-        app.quit();
-    }
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
